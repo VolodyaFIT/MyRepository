2e072d89b7870079e568a99d48499129fa10a336
VolodyaFIT <38290231+VolodyaFIT@users.noreply.github.com>
2021-05-24 16:51:14 +0700

    Create app-routing.module.ts

---
+++ MyProject/src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { AddLessonComponent } from './add-lesson/add-lesson.component';
+import { AuthGuard } from './auth.guard';
+import { FirstCompComponent } from './components/first-comp/first-comp.component';
+//import { NgrxLessonsComponent } from './ngrx-lessons/ngrx-lessons.component';
+import { SecondCompComponent } from './second-comp/second-comp.component';
+
+const routes: Routes = [
+  {
+    path : '',
+    component : FirstCompComponent
+  },
+  {
+    path : 'content',
+    component : SecondCompComponent,
+  },
+  {
+    path : 'add_lesson',
+    component : AddLessonComponent,
+  },
+  {
+    path : 'ngrx',
+    //component : NgrxLessonsComponent,
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
